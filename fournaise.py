eb0687beacd7c342314d9877986592b773aff607
Soyras <74102984+Soyras@users.noreply.github.com>
2020-11-17 18:36:02 +0100

    consigne B

--- fournaise.py
+++ fournaise.py
@@ -1,6 +1,16 @@
 import os
 
-fichier = open("messagePourSauverLeMonde.txt", "r")
-for mot in fichier:
-    if 'volcan' in mot:
+sauver = open("messagePourSauverLeMonde.txt", "r")
+
+# Affiche une message une fois le mot trouvé
+for line in sauver:
+    if 'volcan' in line:
         print ("A l'aide, tous aux abris!")
+sauver.close()
+
+# Affiche une message une fois le mot trouvé
+nature = open("livreDeLaNatureEtDesLacs.txt", "r")
+for ligne in nature:
+    if 'arbre' in ligne:
+        print(f"Nous pouvons nous abriter à {ligne} pas d'ici, dans cet arbre suspendu!")
+nature.close()
